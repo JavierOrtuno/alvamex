1c804c93766f6860b39b1059ab57fdf61925a7be
Fco. Javier Ortuño Colchado <isc.fco.javier.ortuno@gmail.com>
2016-04-29 00:32:44 -0500

    Initial import

---
+++ alvamex/src/main/java/com/frogensource/alvamex/config/Config.java
@@ -0,0 +1,37 @@
+package com.frogensource.alvamex.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+
+@Configuration
+@ComponentScan(
+	{
+		"com.frogensource.alvamex.config",
+		"com.frogensource.alvamex.controller"
+	})
+@EnableWebMvc
+public class Config extends WebMvcConfigurerAdapter {
+
+	@Bean
+	public UrlBasedViewResolver setupViewResolver() {
+        UrlBasedViewResolver resolver = new UrlBasedViewResolver();
+        resolver.setPrefix("/view/");
+        resolver.setSuffix(".jsp");
+        resolver.setViewClass(JstlView.class);
+        return resolver;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/*");
+    }
+
+}
---
+++ alvamex/src/main/webapp/WEB-INF/view/home.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Welcome HOME ALVAMEX</h2>
+</body>
+</html>
